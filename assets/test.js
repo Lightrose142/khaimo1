99d09b6026e744a52014ed6598833b9d9b8d4f1f
Lightrose142 <92564285+Lightrose142@users.noreply.github.com>
2022-07-06 20:27:23 +0700

    new landingpage

---
+++ assets/test.js
@@ -0,0 +1,44 @@
+const firstSection = document.getElementById('firstSection')
+
+console.log(firstSection)
+const beforeFirstSection = document.createElement('section')
+
+beforeFirstSection.classList.add('section hello pwepwe')
+
+beforeFirstSection.style.fontSize = '2rem'
+
+beforeFirstSection.textContent = 'Hello this is added by javascrip'
+
+firstSection.before(beforeFirstSection)
+
+console.log(beforeFirstSection)
+
+function createElement(element, classList) {
+
+    element = document.createElement(element)
+
+    classList.forEach(className => {
+
+        element.classList.add(`${className}`)
+    })
+
+    const styles = {
+        style: 'font-size: 50px; color: red; font-weight: 300'
+        
+    }
+    
+    for (let test in styles) {
+        console.log(`${test}: ${styles[test]}`)
+        element.setAttribute(test, styles[test])
+    }
+
+    return element
+
+}
+
+const elementTest = createElement('h1', ['section', 'test1', 'test2'])
+
+elementTest.textContent = 'Hello this is added by javascrip'
+
+firstSection.before(elementTest)
+
